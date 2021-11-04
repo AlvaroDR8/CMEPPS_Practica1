9248d7109eb64d759d631bf0dad452572e5008df
Aburo Senpai <Aburo Senpai@EquipodeAburo.technicolor.net>
2021-11-04 21:18:25 +0100

    5-Branch

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -63,7 +63,7 @@ public class Anagrams extends JFrame {
                 new Anagrams().setVisible(true);
             }
         });
-        System.out.println("Version 4-Branch(Merged)");
+        System.out.println("Version 5-Branch");
     }
 
     private int wordIdx = 0;
